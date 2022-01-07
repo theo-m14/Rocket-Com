a9b00a53aafbba5e0bbcb2cb3e7ab90ee63b4503
NeylonR <rodrigues.neylon.0@gmail.com>
2022-01-07 16:32:45 +0100

    rajout animation chevron, couleur socials header, modification d'animation, optimisation resolution et poid des images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const reveals = document.querySelectorAll(".reveal");
 
-
-
 function animationFade(){
     for(let i=0; i < reveals.length; i++) {
+    // la hauteur de la fenetre
     const windowHeight = window.innerHeight;
     let elementTop = reveals[i].getBoundingClientRect().top;
+    console.log(elementTop+"oui");
+    // la taille de ce qu'on voit de l'element
     const elementVisible = 20;
 
     if(elementTop < windowHeight - elementVisible){
